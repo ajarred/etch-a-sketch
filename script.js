3e0d246a326050e10cd8b15c830b2b3050db92d3
ajarred <adjarredtan@gmail.com>
2023-05-09 14:34:27 +0800

    Add adjusting grid size

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.createElement('div');
     container.classList.add('container');
-  
+    
     document.body.appendChild(container);
+    
+    createGrid(16); 
+    
+    const resetButton = document.createElement('button');
+    resetButton.textContent = 'Reset Grid';
+    resetButton.addEventListener('click', resetGrid);
+    
+    document.body.insertBefore(resetButton, container);
+  });
   
-    for (let i = 0; i < 16; i++) {
-      for (let j = 0; j < 16; j++) {
+  function createGrid(squaresPerSide) {
+    const container = document.querySelector('.container');
+    container.innerHTML = ''; 
+    
+    const squareSize = 960 / squaresPerSide;
+    
+    container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`; 
+    
+    for (let i = 0; i < squaresPerSide; i++) {
+      for (let j = 0; j < squaresPerSide; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        
+        square.style.width = squareSize + 'px';
+        square.style.height = squareSize + 'px';
+    
         square.addEventListener('mouseover', function() {
           square.style.backgroundColor = getRandomColor();
         });
-        
+    
         container.appendChild(square);
       }
     }
-  });
+  }
   
+  function resetGrid() {
+    let squaresPerSide = prompt('Enter the number of squares per side (maximum 100):');
+    squaresPerSide = parseInt(squaresPerSide);
+    
+    if (isNaN(squaresPerSide) || squaresPerSide <= 0 || squaresPerSide > 100) {
+      alert('Invalid input. Please enter a number between 1 and 100.');
+      return;
+    }
+    
+    createGrid(squaresPerSide);
+  }
+  
+
   function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
