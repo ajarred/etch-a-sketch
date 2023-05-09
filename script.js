33aa9e8ad9f4107843630f57649a891fc6151f42
ajarred <adjarredtan@gmail.com>
2023-05-09 13:57:35 +0800

    Add hover changing color effect

--- script.js
+++ script.js
@@ -8,7 +8,22 @@ document.addEventListener('DOMContentLoaded', function() {
       for (let j = 0; j < 16; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        
+        square.addEventListener('mouseover', function() {
+          square.style.backgroundColor = getRandomColor();
+        });
+        
         container.appendChild(square);
       }
     }
-  });  
+  });
+  
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
